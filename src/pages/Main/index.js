0c802a89b8d72a473a1d683f6ca2fc767f186c1a
Daniel <dantxal@gmail.com>
2019-10-14 09:38:19 -0300

    Initial structure: routes, import, eslint

---
+++ src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>I&apos;M DANIEL TEIXEIRA</h1>
+    </Container>
+  );
+}
