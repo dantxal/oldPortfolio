8fc8b0ac1b80460af5184be284132ef3d555a502
dantxal <dantxal@gmail.com>
2020-10-14 17:10:33 -0300

    Adds Privacy Policy route for Biblia app

---
+++ src/pages/Biblia/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  strong {
+    margin-top: 20px;
+    margin-bottom: 5px;
+  }
+`
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-import Main from '~/pages/Main';
-import Whats from '~/pages/Whats';
+import Main from '~/pages/Main'
+import Whats from '~/pages/Whats'
+import Biblia from '~/pages/Biblia'
 
-export default function Routes() {
+export default function Routes () {
   return (
     <Switch>
+      <Route path="/bibliapp" component={Biblia} />
       <Route path="/whats" component={Whats} />
-      <Route path="/" component={Main} />
+      <Route exact path="/" component={Main} />
     </Switch>
-  );
+  )
 }
